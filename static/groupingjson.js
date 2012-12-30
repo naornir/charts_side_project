bf678f887a73d6f60b4d9d4631e30b8d5736054a
Nir Naor <nirnaori@gmail.com>
2012-12-30 15:02:24 +0200

    added groupingjson to the static folder

---
+++ static/groupingjson.js
@@ -0,0 +1,631 @@
+{
+    "_id": "50e0379712a4fd2dc0000004", 
+    "args": {
+        "cols": [
+            {
+                "attr": "fact", 
+                "title": "fact"
+            }, 
+            {
+                "attr": "install type", 
+                "title": "install type"
+            }
+        ], 
+        "filters": [], 
+        "measures": [
+            {
+                "agg": "sum", 
+                "attrs": [
+                    {
+                        "attr": "winning amount", 
+                        "title": "winning amount"
+                    }
+                ], 
+                "calc": null, 
+                "filter": [], 
+                "timespan": "", 
+                "title": "Winning Amount"
+            }, 
+            {
+                "agg": "count", 
+                "attrs": [
+                    {
+                        "attr": "session_id", 
+                        "title": "session_id"
+                    }
+                ], 
+                "calc": null, 
+                "filter": [], 
+                "timespan": "", 
+                "title": "Session Count"
+            }
+        ], 
+        "rows": [
+            {
+                "attr": "application", 
+                "title": "application"
+            }, 
+            {
+                "attr": "gender", 
+                "title": "gender"
+            }
+        ], 
+        "sort": [
+            {
+                "attr": "application", 
+                "title": "application"
+            }, 
+            {
+                "attr": "gender", 
+                "title": "gender"
+            }
+        ], 
+        "timebreakdown": {
+            "value": "none"
+        }, 
+        "timeframe": {
+            "value": "last30days"
+        }
+    }, 
+    "created_on": {
+        "$date": 1356871575701
+    }, 
+    "elapsed": 1.0, 
+    "file": null, 
+    "modified_on": {
+        "$date": 1356871575902
+    }, 
+    "out": "report_metrics__b3d786b5dd592d38a312f36dca694d57_0", 
+    "ref": "b3d786b5dd592d38a312f36dca694d57", 
+    "results": {
+        "count": 14, 
+        "data": [
+            [
+                "Guy", 
+                null, 
+                "Session", 
+                null
+            ], 
+            [
+                "Guy", 
+                null, 
+                "user", 
+                null
+            ], 
+            [
+                "SomeSlotsGame", 
+                null, 
+                "Scatter", 
+                null
+            ], 
+            [
+                "SomeSlotsGame", 
+                null, 
+                "install", 
+                null
+            ], 
+            [
+                "SomeSlotsGame", 
+                null, 
+                "spin", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "Bet", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "Bonus", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "Scatter", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "Session", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "install", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "spin", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                null, 
+                "user", 
+                null
+            ], 
+            [
+                "slots_craze", 
+                "female", 
+                "User", 
+                "Mobile"
+            ], 
+            [
+                "slots_craze", 
+                "male", 
+                "User", 
+                "Mobile"
+            ]
+        ], 
+        "headers": [
+            [
+                {
+                    "rows": 4, 
+                    "value": "application"
+                }, 
+                {
+                    "rows": 4, 
+                    "value": "gender"
+                }, 
+                {
+                    "cols": 32, 
+                    "value": "fact,install type"
+                }
+            ], 
+            [
+                {
+                    "cols": 4, 
+                    "value": "Session"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "user"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "Scatter"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "install"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "spin"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "Bet"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "Bonus"
+                }, 
+                {
+                    "cols": 4, 
+                    "value": "User"
+                }
+            ], 
+            [
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }, 
+                {
+                    "cols": 2, 
+                    "value": null
+                }, 
+                {
+                    "cols": 2, 
+                    "value": "Mobile"
+                }
+            ], 
+            [
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Winning Amount"
+                }, 
+                {
+                    "cols": 1, 
+                    "value": "Session Count"
+                }
+            ]
+        ], 
+        "limit": 100, 
+        "page": 1, 
+        "pages": 1.0, 
+        "params": [
+            "application", 
+            "gender", 
+            "fact", 
+            "install type"
+        ], 
+        "parsed_data": [
+            [
+                {
+                    "rows": 1, 
+                    "value": "Guy"
+                }, 
+                {
+                    "rows": 1, 
+                    "value": null
+                }, 
+                0.0, 
+                10.0, 
+                null, 
+                null, 
+                0.0, 
+                4.0, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null
+            ], 
+            [
+                {
+                    "rows": 1, 
+                    "value": "SomeSlotsGame"
+                }, 
+                {
+                    "rows": 1, 
+                    "value": null
+                }, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                1.0, 
+                null, 
+                null, 
+                null, 
+                1.0, 
+                null, 
+                null, 
+                0.0, 
+                2.0, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null
+            ], 
+            [
+                {
+                    "rows": 3, 
+                    "value": "slots_craze"
+                }, 
+                {
+                    "rows": 1, 
+                    "value": null
+                }, 
+                0.0, 
+                4.0, 
+                null, 
+                null, 
+                0.0, 
+                14.0, 
+                null, 
+                null, 
+                0.0, 
+                3.0, 
+                null, 
+                null, 
+                0.0, 
+                3.0, 
+                null, 
+                null, 
+                0.0, 
+                6.0, 
+                null, 
+                null, 
+                -214120.0, 
+                140.0, 
+                null, 
+                null, 
+                0.0, 
+                4.0, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null
+            ], 
+            [
+                {
+                    "rows": 1, 
+                    "value": "female"
+                }, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                1.0
+            ], 
+            [
+                {
+                    "rows": 1, 
+                    "value": "male"
+                }, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                null, 
+                1.0
+            ]
+        ], 
+        "skip": 0
+    }, 
+    "status": "success", 
+    "type": "Metrics"
+}
